cae36e52a0debbacee3b2aa651df4765697cefdd
fionaliu66 <fiona.liu0606@gmail.com>
2024-03-07 14:14:34 +0100

    Added test project

---
+++ Uppgift3/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift3
+{
+    internal class Person
+    {
+        private int _age;
+        private string _fName;
+        private string _lName;
+        public int Age {
+            get { return _age; }
+            set {
+                if(value > 0) _age = value;
+            }
+        }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+
+
+
+    }
+}
