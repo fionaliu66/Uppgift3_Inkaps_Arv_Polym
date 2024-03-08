9048a12722b5eebdf79342d3efb3060c335e56df
fionaliu66 <fiona.liu0606@gmail.com>
2024-03-08 15:48:03 +0100

    List of Dogs

--- Uppgift3/Program.cs
+++ Uppgift3/Program.cs
@@ -5,8 +5,11 @@ namespace Uppgift3
     {
         private static List<UserError> userErrors = new();
         private static List<Animal.Animal> animals = new();
+        private static List<Dog> dogs = new();
         static void Main(string[] args)
         {
+            //Person class test
+            //***********************************************
             //PersonHandler personHandler = new PersonHandler();
             //try
             //{
@@ -14,7 +17,10 @@ namespace Uppgift3
             //}catch (ArgumentException ex) { 
             //    Console.WriteLine(ex.Message);
             //}
+            //***********************************************
 
+            //User error test
+            //***********************************************
             //userErrors.Add(new NullInputError());
             //userErrors.Add(new NumericInputError());
             //userErrors.Add(new TextInputError());
@@ -25,45 +31,61 @@ namespace Uppgift3
             //{
             //    Console.WriteLine(error.UEMessage());
             //}
+            //***********************************************
 
-            Bird bird = new Bird("Bird", 0.8, 1, true);
-            Dog dog = new Dog("Dog", 13, 2, "Tax");
-            Flamingo flamingo = new Flamingo("Flamingo", 13, 2, true, 13);
-            Hedgehog hedgehog = new Hedgehog("Hedgehog", 0.5, 1, 100);
+            //Animal list loop test
+            //***********************************************
+            //Bird bird = new Bird("Bird", 0.8, 1, true);
+            //Dog dog = new Dog("Dog", 13, 2, "Tax");
+            //Flamingo flamingo = new Flamingo("Flamingo", 13, 2, true, 13);
+            //Hedgehog hedgehog = new Hedgehog("Hedgehog", 0.5, 1, 100);
+            //Horse horse = new Horse("Hourse", 50, 5, 1.2);
+            //Pelican pelican = new Pelican("Pelican", 10, 3, true, 10);
+            //Swan swan = new Swan("Swan", 3, 2, true, 20);
+            //Wolf wolf = new Wolf("Wolf", 35, 3, 13);
+            //Wolfman wolfman = new Wolfman("Daniel", 80, 120, 8, 190);
+            //Worm worm = new Worm("Worm", 0.1, 3, true);
+
+            //animals.Add(bird);
+            //animals.Add(dog);
+            //animals.Add(flamingo);
+            //animals.Add(hedgehog);
+            //animals.Add(horse);
+            //animals.Add(pelican);
+            //animals.Add(swan);
+            //animals.Add(wolf);
+            //animals.Add(wolfman);
+            //animals.Add(worm);
+
+            //foreach (var animal in animals)
+            //{
+            //    Console.WriteLine(animal.GetType().Name);
+            //    if (animal is Wolfman castedToWolfman)
+            //    {
+            //        Console.WriteLine(castedToWolfman.Talk()); 
+            //    }
+            //    else
+            //    {
+            //        animal.DoSound();
+            //    }
+
+
+            //}
+            //***********************************************
+
+
+            Dog dog1 = new Dog("Buddy", 12.5, 3, "Labrador");
+            Dog dog2 = new Dog("Max", 8.9, 5, "German Shepherd");
+            Dog dog3 = new Dog("Bella", 6.2, 2, "Golden Retriever");
+            Dog dog4 = new Dog("Charlie", 9.8, 4, "Poodle");
+            Dog dog5 = new Dog("Lucy", 7.5, 6, "Beagle");
             Horse horse = new Horse("Hourse", 50, 5, 1.2);
-            Pelican pelican = new Pelican("Pelican", 10, 3, true, 10);
-            Swan swan = new Swan("Swan", 3, 2, true, 20);
-            Wolf wolf = new Wolf("Wolf", 35, 3, 13);
-            Wolfman wolfman = new Wolfman("Daniel", 80, 120, 8, 190);
-            Worm worm = new Worm("Worm", 0.1, 3, true);
-
-            animals.Add(bird);
-            animals.Add(dog);
-            animals.Add(flamingo);
-            animals.Add(hedgehog);
-            animals.Add(horse);
-            animals.Add(pelican);
-            animals.Add(swan);
-            animals.Add(wolf);
-            animals.Add(wolfman);
-            animals.Add(worm);
-
-            foreach (var animal in animals)
-            {
-                Console.WriteLine(animal.GetType().Name);
-                if (animal is Wolfman castedToWolfman)
-                {
-                    Console.WriteLine(castedToWolfman.Talk()); 
-                }
-                else
-                {
-                    animal.DoSound();
-                }
-
-
-            }
-
-
+            dogs.Add(dog1); 
+            dogs.Add(dog2);
+            dogs.Add(dog3);
+            dogs.Add(dog4);
+            dogs.Add(dog5);
+            //dogs.Add((Dog)horse); Cannot Convert from Horse to Dog
         }
 
 
